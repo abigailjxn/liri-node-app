016eff1eaf31c6d60088dbd224f7900a7579863c
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-11-05 18:32:56 -0600

    created switch statement for different inputs

--- liri.js
+++ liri.js
@@ -2,20 +2,26 @@
 require("dotenv").config();
 
 //Add the code required to import the `keys.js` file and store it in a variable.
-const keys = require("./keys")
+const keys = require("./keys");
 console.log(keys);
 
 //You should then be able to access your keys information like so
-
+const Spotify = require("node-spotify-api");
 
 const spotify = new Spotify(keys.spotify);
 console.log(spotify);
 
 //Make it so liri.js can take in one of the following commands:
-// `concert-this`
+let userCommand = process.argv[2];
+let searchQuery = process.argv[3];
 
-//  `spotify-this-song`
-
-//  `movie-this`
-
-//  `do-what-it-says`
+switch (userCommand) {
+  case "concert-this":
+    break;
+  case "spotify-this-song":
+    break;
+  case "movie-this":
+    break;
+  case "do-what-it-says":
+    break;
+}
