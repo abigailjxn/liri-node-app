856ab5f7ab29f469a1627f7d4498f48d053a6a73
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-11-07 17:02:43 -0600

    changed query variable names for specificity

--- liri.js
+++ liri.js
@@ -16,17 +16,18 @@ let userCommand = process.argv[2];
 let searchQuery = process.argv[3];
 let request = require("request");
 
-switch (userCommand){ 
+switch (userCommand) {
+  // BANDSINTOWN CASE
   case "concert-this":
     // add validation later -- The name of the artist. If it contains one of the special characters below, please be sure to replace it by the corresponding code: for / use %252F, for ? use %253F, for * use %252A, and for " use %27C
     let artist = searchQuery;
-    let queryUrl =
+    let bandsQueryUrl =
       "https://rest.bandsintown.com/artists/" +
       artist +
       "/events?app_id=codingbootcamp";
     var moment = require("moment");
     moment().format();
-    request(queryUrl, function(error, response, body) {
+    request(bandsQueryUrl, function(error, response, body) {
       let concertDate = moment(JSON.parse(body)[0].datetime).calendar();
       let concertTime = moment(JSON.parse(body)[0].datetime).format("h:mm a");
       if (!error && response.statusCode === 200) {
@@ -44,35 +45,12 @@ switch (userCommand){
       }
     });
     break;
+
+  // SPOTIFY CASE
   case "spotify-this-song":
     let songTitle = searchQuery;
-    // if (songTitle == undefined) {
-    //   console.log("it was undefined");
-    //   songTitle = "thesign"
-    //   spotify.search({ type: "track", query: songTitle, limit: 1 }, function(
-    //     err,
-    //     data
-    //   ) {
-    //     if (err) {
-    //       return console.log("Error occurred: " + err);
-    //     }
-      
-    //       let artist = data.tracks.items[i].artists[0].name;
-    //       let songName = data.tracks.items[i].name;
-    //       let prevLink = data.tracks.items[i].album.external_urls.spotify;
-    //      let album = data.tracks.items[i].album.name;
-  
-    //       console.log(`
-    //       --------------------
-  
-    //       Track Title: ${songName}
-    //       Artist: ${artist}
-    //       Album: ${album}
-    //       Spotify Preview: ${prevLink}
-          
-    //       --------------------
-    //       `);
-    // });
+    // add if (songTitle == undefined) case
+    // also add user validation for multi-word songTitle
     spotify.search({ type: "track", query: songTitle, limit: 3 }, function(
       err,
       data
@@ -104,9 +82,46 @@ switch (userCommand){
       }
     });
     break;
-//   case "movie-this":
+
+  // OMDB CASE
+  case "movie-this":
+  // add undefined "MR.Nobody Case"
+  // add multi-word movie titles
+    let movieTitle = searchQuery;
+    let movieQueryUrl =
+      "http://www.omdbapi.com/?apikey=trilogy&t=" +
+      movieTitle +
+      "&y=&plot=short";
+    request(movieQueryUrl, function(error, response, body) {
+      if (error) {
+        console.log("Error occurred: " + error);
+      } else if (!error && response.statusCode === 200) {
+        let movieTitle = JSON.parse(body).Title;
+        let releaseYear = JSON.parse(body).Year;
+        let plot = JSON.parse(body).Plot;
+        let actors = JSON.parse(body).Actors;
+        let imdbRating = JSON.parse(body).imdbRating;
+        let rotTomRating = JSON.parse(body).Ratings[1].Value;
+        let productionCountry = JSON.parse(body).Country;
+        let language = JSON.parse(body).Language;
+
+        console.log(`
+      --------------------
+      
+      Movie Title: ${movieTitle}
+      Release Year: ${releaseYear}
+      Plot Summary: ${plot}
+      Cast: ${actors}
+      IMDB Rating: ${imdbRating}
+      Rotten Tomatoes Rating: ${rotTomRating}
+      Produced in: ${productionCountry}
+      Language ${language}
+      
+      --------------------
+      `);
+      }
+    });
     break;
-//   case "do-what-it-says":
+  case "do-what-it-says":
     break;
-// }
 }
