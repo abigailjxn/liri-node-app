3db8fb51038136659318b53e3da1212515be51fc
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-11-07 15:43:24 -0600

    spotify working for user input cases, not for undefined cases

--- liri.js
+++ liri.js
@@ -15,9 +15,8 @@ console.log(spotify);
 let userCommand = process.argv[2];
 let searchQuery = process.argv[3];
 let request = require("request");
-// var day = moment("1995-12-25");
 
-switch (userCommand) {
+switch (userCommand){ 
   case "concert-this":
     // add validation later -- The name of the artist. If it contains one of the special characters below, please be sure to replace it by the corresponding code: for / use %252F, for ? use %253F, for * use %252A, and for " use %27C
     let artist = searchQuery;
@@ -28,8 +27,8 @@ switch (userCommand) {
     var moment = require("moment");
     moment().format();
     request(queryUrl, function(error, response, body) {
-        let concertDate = moment(JSON.parse(body)[0].datetime).calendar();
-        let concertTime = moment(JSON.parse(body)[0].datetime).format("h:mm a");
+      let concertDate = moment(JSON.parse(body)[0].datetime).calendar();
+      let concertTime = moment(JSON.parse(body)[0].datetime).format("h:mm a");
       if (!error && response.statusCode === 200) {
         console.log(`
 
@@ -46,9 +45,68 @@ switch (userCommand) {
     });
     break;
   case "spotify-this-song":
+    let songTitle = searchQuery;
+    // if (songTitle == undefined) {
+    //   console.log("it was undefined");
+    //   songTitle = "thesign"
+    //   spotify.search({ type: "track", query: songTitle, limit: 1 }, function(
+    //     err,
+    //     data
+    //   ) {
+    //     if (err) {
+    //       return console.log("Error occurred: " + err);
+    //     }
+      
+    //       let artist = data.tracks.items[i].artists[0].name;
+    //       let songName = data.tracks.items[i].name;
+    //       let prevLink = data.tracks.items[i].album.external_urls.spotify;
+    //      let album = data.tracks.items[i].album.name;
+  
+    //       console.log(`
+    //       --------------------
+  
+    //       Track Title: ${songName}
+    //       Artist: ${artist}
+    //       Album: ${album}
+    //       Spotify Preview: ${prevLink}
+          
+    //       --------------------
+    //       `);
+    // });
+    spotify.search({ type: "track", query: songTitle, limit: 3 }, function(
+      err,
+      data
+    ) {
+      if (err) {
+        return console.log("Error occurred: " + err);
+      }
+      let artist = "";
+      let songName = "";
+      let prevLink = "";
+      let album = "";
+      for (let i = 0; i < data.tracks.items.length; i++) {
+        artist = data.tracks.items[i].artists[0].name;
+        songName = data.tracks.items[i].name;
+        prevLink = data.tracks.items[i].album.external_urls.spotify;
+        album = data.tracks.items[i].album.name;
+
+        // console.log(artist, songName, prevLink, album);
+        console.log(`
+        --------------------
+
+        Track Title: ${songName}
+        Artist: ${artist}
+        Album: ${album}
+        Spotify Preview: ${prevLink}
+        
+        --------------------
+        `);
+      }
+    });
     break;
-  case "movie-this":
+//   case "movie-this":
     break;
-  case "do-what-it-says":
+//   case "do-what-it-says":
     break;
+// }
 }
