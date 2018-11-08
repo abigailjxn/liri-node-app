61e3cc8665e9f93221ea5555f85f6f6c9718b5ae
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2018-11-08 00:25:37 -0600

    added functions to encase api calls, added functions to cases

--- liri.js
+++ liri.js
@@ -13,64 +13,61 @@ console.log(spotify);
 
 //Make it so liri.js can take in one of the following commands:
 let userCommand = process.argv[2];
-let searchQuery = process.argv.slice(3).join(' ');
+let searchQuery = process.argv.slice(3).join(" ");
 let request = require("request");
 
-switch (userCommand) {
-  // BANDSINTOWN CASE
-  case "concert-this":
-    // add validation later -- The name of the artist. If it contains one of the special characters below, please be sure to replace it by the corresponding code: for / use %252F, for ? use %253F, for * use %252A, and for " use %27C
-    let artist = searchQuery;
-    let bandsQueryUrl =
-      "https://rest.bandsintown.com/artists/" +
-      artist +
-      "/events?app_id=codingbootcamp";
-    var moment = require("moment");
-    moment().format();
-    request(bandsQueryUrl, function(error, response, body) {
-      let concertDate = moment(JSON.parse(body)[0].datetime).calendar();
-      let concertTime = moment(JSON.parse(body)[0].datetime).format("h:mm a");
-      if (!error && response.statusCode === 200) {
-        console.log(`
+function concertSearch(searchQuery) {
+  // add validation later -- The name of the artist. If it contains one of the special characters below, please be sure to replace it by the corresponding code: for / use %252F, for ? use %253F, for * use %252A, and for " use %27C
+  let artist = searchQuery;
+  let bandsQueryUrl =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=codingbootcamp";
+  var moment = require("moment");
+  moment().format();
+  request(bandsQueryUrl, function(error, response, body) {
+    let concertDate = moment(JSON.parse(body)[0].datetime).calendar();
+    let concertTime = moment(JSON.parse(body)[0].datetime).format("h:mm a");
+    if (!error && response.statusCode === 200) {
+      console.log(`
 
-            --------------------
+         --------------------
 
-            ${artist} will be playing at ${JSON.parse(body)[0].venue.name}
-            in ${JSON.parse(body)[0].venue.city}
-            on ${concertDate} at ${concertTime}.
-           
-            --------------------
+         ${artist} will be playing at ${JSON.parse(body)[0].venue.name}
+         in ${JSON.parse(body)[0].venue.city}
+         on ${concertDate} at ${concertTime}.
+        
+         --------------------
 
-            `);
-      }
-    });
-    break;
+         `);
+    }
+  });
+}
 
-  // SPOTIFY CASE
-  case "spotify-this-song":
-    let songTitle = searchQuery;
-    // add if (songTitle == undefined) case
-    // add if (!songTitle)
-    // also add user validation for multi-word songTitle
-    spotify.search({ type: "track", query: songTitle, limit: 3 }, function(
-      err,
-      data
-    ) {
-      if (err) {
-        return console.log("Error occurred: " + err);
-      }
-      let artist = "";
-      let songName = "";
-      let prevLink = "";
-      let album = "";
-      for (let i = 0; i < data.tracks.items.length; i++) {
-        artist = data.tracks.items[i].artists[0].name;
-        songName = data.tracks.items[i].name;
-        prevLink = data.tracks.items[i].album.external_urls.spotify;
-        album = data.tracks.items[i].album.name;
+function spotifySearch(searchQuery) {
+  let songTitle = searchQuery;
+  // add if (songTitle == undefined) case
+  // add if (!songTitle)
 
-        // console.log(artist, songName, prevLink, album);
-        console.log(`
+  spotify.search({ type: "track", query: songTitle, limit: 3 }, function(
+    err,
+    data
+  ) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+    let artist = "";
+    let songName = "";
+    let prevLink = "";
+    let album = "";
+    for (let i = 0; i < data.tracks.items.length; i++) {
+      artist = data.tracks.items[i].artists[0].name;
+      songName = data.tracks.items[i].name;
+      prevLink = data.tracks.items[i].album.external_urls.spotify;
+      album = data.tracks.items[i].album.name;
+
+      // console.log(artist, songName, prevLink, album);
+      console.log(`
         --------------------
 
         Track Title: ${songName}
@@ -80,14 +77,11 @@ switch (userCommand) {
         
         --------------------
         `);
-      }
-    });
-    break;
+    }
+  });
+}
 
-  // OMDB CASE
-  case "movie-this":
-  // add undefined "MR. Nobody Case"
-  // add multi-word movie titles
+function omdbSearch (searchQuery) {
     let movieTitle = searchQuery;
     let movieQueryUrl =
       "http://www.omdbapi.com/?apikey=trilogy&t=" +
@@ -116,15 +110,82 @@ switch (userCommand) {
       IMDB Rating: ${imdbRating}
       Rotten Tomatoes Rating: ${rotTomRating}
       Produced in: ${productionCountry}
-      Language ${language}
+      Language: ${language}
       
       --------------------
       `);
       }
     });
+}
+
+// SWITCH START
+
+switch (userCommand) {
+  // BANDSINTOWN CASE
+  case "concert-this":
+    if (!searchQuery) {
+      concertSearch("The Japanese House");
+    } else {
+      concertSearch(searchQuery);
+    }
     break;
+
+  // SPOTIFY CASE
+  case "spotify-this-song":
+    if (!searchQuery) {
+      if (!searchQuery) {
+        spotify.search({ type: "track", query: "The Sign", limit: 9 }, function(
+          err,
+          data
+        ) {
+          if (err) {
+            return console.log("Error occurred: " + err);
+          }
+          let artist = data.tracks.items[8].artists[0].name;
+          let songName = data.tracks.items[8].name;
+          let prevLink = data.tracks.items[8].album.external_urls.spotify;
+          let album = data.tracks.items[8].album.name;
+
+          console.log(`
+                    --------------------
+            
+                    Track Title: ${songName}
+                    Artist: ${artist}
+                    Album: ${album}
+                    Spotify Preview: ${prevLink}
+                    
+                    --------------------
+                    `);
+        });
+      } else {
+        spotifySearch(searchQuery);
+      }
+    }
+    break;
+
+  // OMDB CASE
+  case "movie-this":
+    if (!searchQuery) {
+        omdbSearch("Mr. Nobody");
+      } else {
+        omdbSearch(searchQuery);
+      }
+    break;
+
+  // RANDOM.TXT CASE
   case "do-what-it-says":
-  let file = require('file-system');
-//   file
+    let file = require("file-system");
+    let text = file.readFileSync("./random.txt", "utf8");
+    console.log(text);
+    let textData = text.split(",");
+    let textCommand = textData[0];
+    let textQuery = textData[1];
+    console.log(textCommand, textQuery);
+    switch(textCommand) {
+        case "spotify-this-song":
+        spotifySearch(textQuery);
+        break;
+    }
+
     break;
 }
